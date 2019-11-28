8bdaec964df444c920c6d61cb84c4cd16eab053a
Al farizi <lionelandresfariz@gmail.com>
2019-11-29 01:38:21 +0700

    nice, but need check for the last read, because of in.good()

--- MalikaLucky/MalikaLucky/Main.cpp
+++ MalikaLucky/MalikaLucky/Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 
 struct Mahasiswa
 {
@@ -20,6 +21,25 @@ private:
 
 int main()
 {
+	std::vector<Mahasiswa> mhs;
+	std::string nama;
+	std::string prodi;
+	std::string strNIU;
+	int intNIU;
+	std::ifstream in("prg9.csv");
+	
+	if (!in.is_open())
+	{
+		std::cout << "Failed load the file" << std::endl;
+	}
+	while (in.good())
+	{
+		std::getline(in, strNIU, ',');
+		intNIU = std::stoi(strNIU);
+		std::getline(in, nama, ',');
+		std::getline(in, prodi, ',');
+		mhs.push_back({ intNIU, nama, prodi });
+	}
 
 	return 0;
 }
